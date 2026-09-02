19524616c2dab0e4612d8b528b697cfca587ead4
HAWKEYE <hawkeye@ovoll.in>
2026-09-02 09:57:20 +0530

    chore: register reminders and queue worker in scheduled console tasks

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,6 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('woof:send-vaccination-reminders')->dailyAt('09:00');
+Schedule::command('reminders:check')->dailyAt('08:30');
 Schedule::command('tickets:notify-stalled')->dailyAt('08:00');
+Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
